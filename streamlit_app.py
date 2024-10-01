b9d7bfdff31a5a898732627903987a3a31b33f1d
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-02 00:08:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,13 @@ st.title("PDF Uploader")
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
 source_type = 'github'
-input_path = 'AR English 2023-24 Final (with cover).pdf'
+with open(os.path.join("tempDir", uploaded_file.name), "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    # Get the full path of the file
+file_path = os.path.join("tempDir", uploaded_file.name)
 additional_metadata = {'key': 'value'}
-texts, metadata_list, langchain_docs = parse_doc_universal(uploaded_file,additional_metadata,source_type, additional_metadata)
+texts, metadata_list, langchain_docs = parse_doc_universal(file_path,additional_metadata,source_type, additional_metadata)
 for i in langchain_docs:
     st.writ(i)
     exit()
